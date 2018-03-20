101f83b7363fef50e3903084c1cf72155cce3b51
Andrew <yo.yoba@yandex.ru>
2018-03-20 17:25:41 +0300

    added EXAMPLE_settings

---
+++ EXAMPLE_settings.py
@@ -0,0 +1,15 @@
+API_TOKEN = "123456789:ABCD123123EFGHIjklMNOpqrstuW"
+
+
+ADMIN_ID = 123456789
+
+
+HELLO_MESSAGE = "Hi"
+NEW_FEEDBACK = "Somebody sent message"
+YES = "Yep"
+NO = "No"
+SURE_TO_FORWARD = "R u sure to send it to admin?"
+NOT_SURE = " not sure"
+ACCEPT = "Accept!"
+REJECT = ":("
+
